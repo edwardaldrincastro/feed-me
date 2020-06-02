0b7dff65f61d10c1988c3a84e63423da6e5a455e
Aldrin <edwardaldrincastro@gmail.com>
2020-06-02 18:35:29 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+interface AppProps {}
+
+const App = (props: AppProps) => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
